96938513ec55e344696d20430d6ce3b57ef6b143
megis <atanmegis.cn@gmail.com>
2023-08-22 09:56:10 +0700

    Revert "test"
    This reverts commit 676f4af51e26685222bc00048f13cc0153f5da0e.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,5 @@
-import styled from 'styled-components'
-import Contact from './components/Contact.jsx'
-import Hero from './components/Hero.jsx'
-import Who from './components/Who.jsx'
-import Works from './components/Works.jsx'
-import { BrowserRouter } from 'react-router-dom'
-
-const Container = styled.div`
-    height: 100vh;
-    scroll-snap-type: y mandatory;
-    scroll-behavior: smooth;
-    overflow-y: auto;
-    scrollbar-width: none;
-    color: white;
-    background: url('./img/bg.jpg');
-    &::-webkit-scrollbar {
-        display: none;
-    }
-`
-
 function App() {
-    return (
-        <BrowserRouter>
-            <Container>
-                <Hero />
-                <Who />
-                <Works />
-                <Contact />
-            </Container>
-        </BrowserRouter>
-    )
+  return <div>Hello World!</div>;
 }
 
-export default App
+export default App;
